657e7ffab37abc913d51a643df0208e7ccb44635
Hitsuji221 <67304907+Hitsuji221@users.noreply.github.com>
2020-11-25 18:16:01 +0900

    フィードバックで直しました。

--- EscapeGame_iPad/Assets/Scripts/QuizGame/Movement.cs
+++ EscapeGame_iPad/Assets/Scripts/QuizGame/Movement.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
     string currentPanelStr = "GameSlide2";
     public GameObject Slide1;
+    public GameObject message6;
+    [SerializeField] AudioClip correct;
+    AudioSource audioSource;
+    
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void StartButton(){
     this.transform.localPosition = new Vector2(-4400,8000);
@@ -15,10 +24,12 @@ public class Movement : MonoBehaviour
     public void Answer(){
     this.transform.localPosition = new Vector2(-4400,9600);
     currentPanelStr = "GameSlide3";
+    audioSource.PlayOneShot(correct);
     }
     public void NextButton(){
     this.transform.localPosition = new Vector2(-4400,11200);
     currentPanelStr = "GameSlide4";
+    message6.SetActive(true);
     }
     public void BackRoom3(){
         this.transform.localPosition = new Vector2(-4400,0);
